7b1805d2697dd73833718a330be29afae1c08c52
jeonseobin <seobin3812@naver.com>
2024-09-05 22:33:33 +0900

    Add files via upload

---
+++ 016_logicalOp/016_logicalOp.cpp
@@ -0,0 +1,16 @@
+﻿// 016_logicalOp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+
+    bool p = a >= 1 && a <= 10;
+    bool q = a == 3 || a == 7;
+    bool r = !q;
+
+    printf("%d %d %d\n", p, q, r);
+}
+
