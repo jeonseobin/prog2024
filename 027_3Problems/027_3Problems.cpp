6c57ce7d28418c6274d9382aca4a6ec23dd4a36e
jeonseobin <seobin3812@naver.com>
2024-09-11 14:54:53 +0900

    Add files via upload

---
+++ 027_3Problems/027_3Problems.cpp
@@ -0,0 +1,51 @@
+﻿// 027_3Problems.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+// 1. 별표를 n개 출력한다
+// 2. 1부터 n까지 홀수를 출력한다.
+// 3. 1부터 n까지 자연수를 모두 더한다
+#include <stdio.h>
+
+int main()
+{
+
+	int n;
+
+	printf("n을 입력 :");
+	scanf_s("%d", &n);
+
+	// while 문(1)
+	int i = 1;
+
+	while (i <= n) {
+		printf("*");
+		i++;
+	}
+	printf("\n");
+
+	// while 문(2)
+	i = 1;
+	
+	while (i <= n) {
+		printf("%d ", i);
+		i += 2;
+	}
+	printf("\n");
+
+	// while 문(3) 1부터 n까지 자연수를 모두 더한다.
+	int sum = 0;
+	i = 1;
+
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("sum = %d\n", sum);
+	// do - while 문
+
+
+	// for 문
+
+}
+
