524274c688373c68eddda9a3792b0dba98994ca0
jeonseobin <seobin3812@naver.com>
2024-11-05 14:41:22 +0900

    Add files via upload

---
+++ 056_bit/056_bit.cpp
@@ -0,0 +1,27 @@
+﻿// 056_bit.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 4;
+
+    printf("a & b = %d\n", a & b);
+    printf("a | b = %d\n", a | b);
+    printf("a ^ b = %d\n", a ^ b);
+    printf("~a = %d\n", ~a);
+    printf("~b = %d\n", ~b);
+    printf("a << 2 = %d\n", a << 2);
+    printf("a >> 2 = %d\n", a >> 2);
+
+    // a &= b;  // a = a % b;
+    // masking
+    int x = 106;    // 0110 1010
+    int mask = 15;  // 0000 1111
+
+    x &= mask;  // x = x & mask;
+    printf("Masking : %d\n", x);
+}
+
